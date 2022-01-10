6b6c415ad9e075fe946f81e90bb9cfcbe800a1d3
adamgian <contact@adamgian.com>
2022-01-10 23:25:58 +1000

    Process directory globs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use glob::glob;
+extern crate globwalk;
+
+use regex::Regex;
 use std::env;
 use std::fs;
 use structopt::StructOpt;
@@ -13,10 +15,27 @@ struct Cli {
 
 fn main() {
 	let args = Cli::from_args();
+	let is_glob_a_directory = !Regex::new(r"\.\{?(\w{2,4},?)+\}?$")
+		.unwrap()
+		.is_match(&args.source);
+	let mut glob_string = String::from(&args.source);
 
-	for entry in glob(&args.source).unwrap() {
+	// If glob string is for a directory
+	if is_glob_a_directory {
+		if glob_string.ends_with("/") {
+			glob_string.push_str("**/*.{css,html,js}");
+		}
+		else if glob_string.ends_with("/*") {
+			glob_string.push_str(".{css,html,js}");
+		}
+		else {
+			glob_string.push_str("/**/*.{css,html,js}");
+		}
+	}
+
+	for entry in globwalk::glob(&glob_string).unwrap() {
 		match entry {
-			Ok(path) => println!("{:?}", path.display()),
+			Ok(file) => println!("{:?}", file.path()),
 			Err(e) => println!("{:?}", e),
 		}
 	}
