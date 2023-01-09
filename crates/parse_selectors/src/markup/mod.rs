c89ce4e025b3720022789e77ef6912ab86b4e068
Adam Gian <contact@adamgian.com>
2023-01-09 20:26:23 +1000

    wip(multi-process)!: port over markup handling

--- crates/parse_selectors/src/markup/mod.rs
+++ crates/parse_selectors/src/markup/mod.rs
@@ -12,51 +12,154 @@ use crate::markup::regexes as markup_regex;
 
 
 
+/// Process HTML.
+pub fn process_html(
+	file_string: &mut String,
+	selectors: &mut Selectors,
+	config: &Config,
+) {
+	process_html_attributes(file_string, selectors, config);
+	process_html_scripts(file_string, selectors, config);
+	process_html_styles(file_string, selectors, config);
+	super::process_prefixed_selectors(file_string, selectors, config);
+}
+
 /// Process HTML attributes.
 pub fn process_html_attributes(
 	file_string: &mut String,
 	selectors: &mut Selectors,
 	config: &Config,
 ) {
-	*file_string = markup_regex::HTML_ATTRIBUTES.replace_all(file_string, |capture: &Captures| {
-		// Matched string is a <code>/<script>/<style> element
-		// or a HTML comment.
-		if capture.at(1).is_none() {
-			return match capture.at(0).unwrap().starts_with("<code") {
-				// <script>/<style> element or HTML comment, leave as is.
-				false => capture.at(0).unwrap().to_string(),
+	if config.current_step == ProcessingSteps::WritingToFiles {
+		handle_file_write(file_string, selectors, config);
+	} else {
+		handle_file_read(file_string, selectors, config);
+	}
 
-				// <code> element.
-				// Check for attributes to encode on the opening tag.
-				true => {
-					let code_element = capture
-						.at(0)
-						.unwrap()
-						.strip_prefix("<code")
-						.unwrap()
-						.split_once('>')
-						.unwrap();
+	fn handle_file_read(
+		file_string: &str,
+		selectors: &mut Selectors,
+		config: &Config,
+	) {
+		for capture in regexes::HTML_ATTRIBUTES.captures_iter(file_string) {
+			// Matched string is a <code>/<script>/<style> element
+			// or a HTML comment.
+			if capture.at(1).is_none() {
+				match capture.at(0).unwrap().starts_with("<code") {
+					// <script>/<style> element or HTML comment, leave as is.
+					false => return,
 
-					let mut code_tag_attributes = code_element.0.to_string();
+					// <code> element.
+					// Check for attributes to encode on the opening tag.
+					true => {
+						let code_element = capture
+							.at(0)
+							.unwrap()
+							.strip_prefix("<code")
+							.unwrap()
+							.split_once('>')
+							.unwrap();
 
-					process_html_attributes(&mut code_tag_attributes, selectors, config);
+						let mut code_tag_attributes = code_element.0.to_string();
 
-					format!(
-						"<code{attributes}>{inner_html}",
-						attributes = code_tag_attributes,
-						inner_html = code_element.1,
-					)
+						process_html_attributes(&mut code_tag_attributes, selectors, config);
+					},
+				};
+			}
+
+			// Attribute does not contain classes and/or IDs.
+			// Leave it as is.
+			if !WHITELIST.contains_key(&capture.at(1).unwrap().to_ascii_lowercase()) {
+				return;
+			}
+
+			let attribute_name: &str = capture.at(1).unwrap();
+			let mut attribute_value: String = unescape_html_chars(capture.at(4).unwrap());
+
+			// Work out if value(s) are classes, IDs or selectors.
+			let attribute_type_designation: &str =
+				WHITELIST.get(&attribute_name.to_ascii_lowercase()).unwrap();
+
+			match attribute_type_designation {
+				"id" | "class" => {
+					super::process_string_of_tokens(
+						&mut attribute_value,
+						selectors,
+						config,
+						attribute_type_designation,
+						SelectorUsage::Identifier,
+					);
 				},
-			};
+
+				"selector" => {
+					super::process_css(&mut attribute_value, selectors, config);
+				},
+
+				"style" => {
+					super::process_css_functions(&mut attribute_value, selectors, config);
+				},
+
+				"script" => {
+					super::process_js(&mut attribute_value, selectors, config);
+				},
+
+				"anchor" => {
+					super::process_anchor_links(&mut attribute_value, selectors, config);
+				},
+
+				_ => return,
+			}
 		}
+	}
 
-		let attribute_name: &str = capture.at(1).unwrap();
-		let attribute_quote: &str = capture.at(3).unwrap_or("");
-		let mut attribute_value: String = unescape_html_chars(capture.at(4).unwrap());
+	fn handle_file_write(
+		file_string: &mut String,
+		selectors: &mut Selectors,
+		config: &Config,
+	) {
+		*file_string =
+			markup_regex::HTML_ATTRIBUTES.replace_all(file_string, |capture: &Captures| {
+				// Matched string is a <code>/<script>/<style> element
+				// or a HTML comment.
+				if capture.at(1).is_none() {
+					return match capture.at(0).unwrap().starts_with("<code") {
+						// <script>/<style> element or HTML comment, leave as is.
+						false => capture.at(0).unwrap().to_string(),
 
-		// Attributes whitelist of which its values should be processed.
-		match WHITELIST.contains_key(&attribute_name.to_ascii_lowercase()) {
-			true => {
+						// <code> element.
+						// Check for attributes to encode on the opening tag.
+						true => {
+							let code_element = capture
+								.at(0)
+								.unwrap()
+								.strip_prefix("<code")
+								.unwrap()
+								.split_once('>')
+								.unwrap();
+
+							let mut code_tag_attributes = code_element.0.to_string();
+
+							process_html_attributes(&mut code_tag_attributes, selectors, config);
+
+							format!(
+								"<code{attributes}>{inner_html}",
+								attributes = code_tag_attributes,
+								inner_html = code_element.1,
+							)
+						},
+					};
+				}
+
+				// Attribute does not contain classes and/or IDs.
+				// Leave it as is.
+				if !WHITELIST.contains_key(&capture.at(1).unwrap().to_ascii_lowercase()) {
+					return capture.at(0).unwrap().to_string();
+				}
+
+				let attribute_name: &str = capture.at(1).unwrap();
+				let attribute_quote: &str = capture.at(3).unwrap_or("");
+				let mut attribute_value: String = unescape_html_chars(capture.at(4).unwrap());
+
 				// Work out if value(s) are classes, IDs or selectors.
 				let attribute_type_designation: &str =
 					WHITELIST.get(&attribute_name.to_ascii_lowercase()).unwrap();
@@ -100,15 +203,8 @@ pub fn process_html_attributes(
 					value = attribute_value,
 					quote = attribute_quote,
 				)
-			},
-
-			// Attribute does not contain classes and/or IDs.
-			// Leave it as is.
-			false => {
-				return capture.at(0).unwrap().to_string();
-			},
-		}
-	});
+			});
+	}
 }
 
 // Process embedded scripts in HTML.
@@ -117,19 +213,41 @@ pub fn process_html_scripts(
 	selectors: &mut Selectors,
 	config: &Config,
 ) {
-	// Create subset string(s) to process each <script> embed.
-	*file_string =
-		markup_regex::HTML_SCRIPT_ELEMENT.replace_all(file_string, |capture: &Captures| {
+	if config.current_step == ProcessingSteps::WritingToFiles {
+		handle_file_write(file_string, selectors, config);
+	} else {
+		handle_file_read(file_string, selectors, config);
+	}
+
+	fn handle_file_read(
+		file_string: &str,
+		selectors: &mut Selectors,
+		config: &Config,
+	) {
+		for capture in regexes::HTML_SCRIPT_ELEMENT.captures_iter(file_string) {
 			let mut embedded_script = capture.at(2).unwrap().to_string();
 			super::process_js(&mut embedded_script, selectors, config);
+		}
+	}
 
-			format!(
-				"{tag_open}{script}{tag_close}",
-				tag_open = capture.at(1).unwrap(),
-				script = embedded_script,
-				tag_close = capture.at(3).unwrap()
-			)
-		});
+	fn handle_file_write(
+		file_string: &mut String,
+		selectors: &mut Selectors,
+		config: &Config,
+	) {
+		*file_string =
+			markup_regex::HTML_SCRIPT_ELEMENT.replace_all(file_string, |capture: &Captures| {
+				let mut embedded_script = capture.at(2).unwrap().to_string();
+				super::process_js(&mut embedded_script, selectors, config);
+
+				format!(
+					"{tag_open}{script}{tag_close}",
+					tag_open = capture.at(1).unwrap(),
+					script = embedded_script,
+					tag_close = capture.at(3).unwrap()
+				)
+			});
+	}
 }
 
 // Processing embedded styles in HTML.
@@ -138,31 +256,41 @@ pub fn process_html_styles(
 	selectors: &mut Selectors,
 	config: &Config,
 ) {
-	// Create subset string(s) to process each <style> embed.
-	*file_string =
-		markup_regex::HTML_STYLE_ELEMENT.replace_all(file_string, |capture: &Captures| {
+	if config.current_step == ProcessingSteps::WritingToFiles {
+		handle_file_write(file_string, selectors, config);
+	} else {
+		handle_file_read(file_string, selectors, config);
+	}
+
+	fn handle_file_read(
+		file_string: &str,
+		selectors: &mut Selectors,
+		config: &Config,
+	) {
+		for capture in regexes::HTML_STYLE_ELEMENT.captures_iter(file_string) {
 			let mut embedded_style = capture.at(2).unwrap().to_string();
 			super::process_css(&mut embedded_style, selectors, config);
+		}
+	}
 
-			format!(
-				"{tag_open}{styles}{tag_close}",
-				tag_open = capture.at(1).unwrap(),
-				styles = embedded_style,
-				tag_close = capture.at(3).unwrap(),
-			)
-		});
-}
+	fn handle_file_write(
+		file_string: &mut String,
+		selectors: &mut Selectors,
+		config: &Config,
+	) {
+		*file_string =
+			markup_regex::HTML_STYLE_ELEMENT.replace_all(file_string, |capture: &Captures| {
+				let mut embedded_style = capture.at(2).unwrap().to_string();
+				super::process_css(&mut embedded_style, selectors, config);
 
-/// Process HTML.
-pub fn process_html(
-	file_string: &mut String,
-	selectors: &mut Selectors,
-	config: &Config,
-) {
-	process_html_attributes(file_string, selectors, config);
-	process_html_scripts(file_string, selectors, config);
-	process_html_styles(file_string, selectors, config);
-	super::process_prefixed_selectors(file_string, selectors, config);
+				format!(
+					"{tag_open}{styles}{tag_close}",
+					tag_open = capture.at(1).unwrap(),
+					styles = embedded_style,
+					tag_close = capture.at(3).unwrap(),
+				)
+			});
+	}
 }
 
 // Convert any escaped chars in HTML substring to UTF8 char.
