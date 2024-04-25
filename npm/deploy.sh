2a759a24df7a96921a8a4af0b52535b1076c2761
Adam Gian <contact@adamgian.com>
2024-04-25 19:28:31 +1000

    fix: paths relative to project root

--- npm/deploy.sh
+++ npm/deploy.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-jq -c '.[]' platforms.json | while read build; do
+jq -c '.[]' npm/platforms.json | while read build; do
 	platform_label=$(jq -r '.platform' <<< "$build")
 	node_platform=$(jq -r '.nodePlatform' <<< "$build")
 	architecture=$(jq -r '.architecture' <<< "$build")
 	binary_label=$(jq -r '.binary' <<< "$build")
 
-	mkdir -p $binary_label
-	cp package.json "./$binary_label/package.json"
+	mkdir -p "npm/$binary_label"
+	cp package.json "npm/$binary_label/package.json"
 
-	sed -i "s/FIXME_VERSION/$1/g" ./$binary_label/package.json
-	sed -i "s/FIXME_BINARY/$binary_label/g" ./$binary_label/package.json
-	sed -i "s/FIXME_PLATFORM/$platform_label/g" ./$binary_label/package.json
-	sed -i "s/FIXME_NODE_PLATFORM/$node_platform/g" ./$binary_label/package.json
-	sed -i "s/FIXME_ARCHITECTURE/$architecture/g" ./$binary_label/package.json
+	sed -i "s/FIXME_VERSION/$1/g" "npm/$binary_label/package.json"
+	sed -i "s/FIXME_BINARY/$binary_label/g" "npm/$binary_label/package.json"
+	sed -i "s/FIXME_PLATFORM/$platform_label/g" "npm/$binary_label/package.json"
+	sed -i "s/FIXME_NODE_PLATFORM/$node_platform/g" "npm/$binary_label/package.json"
+	sed -i "s/FIXME_ARCHITECTURE/$architecture/g" "npm/$binary_label/package.json"
 
 	echo "$binary_label @$1"
 done
