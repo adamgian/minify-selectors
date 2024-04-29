38a5e8ad2ac593906d4f36efaa4ca077a52f04c1
Adam Gian <contact@adamgian.com>
2024-04-29 22:31:29 +1000

    build: add readme and description template

---
+++ npm/README.md
@@ -0,0 +1,16 @@
+[1]: https://github.com/adamgian/minify-selectors/releases/latest
+[2]: https://img.shields.io/npm/v/minify-selectors?color=blue&label=Latest%20release
+[3]: https://www.apache.org/licenses/LICENSE-2.0
+[4]: https://img.shields.io/badge/License-Apache%202.0-green.svg
+
+[![Latest release version][2]][1]&nbsp;
+[![Apache 2.0 license][4]][3]
+
+
+
+
+# minify-selectors
+
+This package contains the pre-built *FIXME_RUST_TARGET* binary for minify-selectors.
+
+For information on how minify-selectors works, please go to the main package: https://www.npmjs.com/package/minify-selectors.
--- npm/deploy.sh
+++ npm/deploy.sh
@@ -6,27 +6,32 @@ jq -c '.[]' npm/platforms.json | while read build; do
 	node_platform=$(jq -r '.nodePlatform' <<< "$build")
 	architecture=$(jq -r '.architecture' <<< "$build")
 	rust_target=$(jq -r '.rustTarget' <<< "$build")
-
-	mkdir -p "npm/$package_name"
-	cp "npm/package.json" "npm/$package_name/package.json"
-
-	sed -i "s/FIXME_VERSION/$1/g" "npm/$package_name/package.json"
-	sed -i "s/FIXME_BINARY/$package_name/g" "npm/$package_name/package.json"
-	sed -i "s/FIXME_PLATFORM/$platform_label/g" "npm/$package_name/package.json"
-	sed -i "s/FIXME_NODE_PLATFORM/$node_platform/g" "npm/$package_name/package.json"
-	sed -i "s/FIXME_ARCHITECTURE/$architecture/g" "npm/$package_name/package.json"
-
 	rust_target_path="merged-artifacts/$rust_target/release/"
 	rust_target_name="minify-selectors"
+
 	if [ "$node_platform" == "win32" ]; then
 		rust_target_name+=".exe"
 	fi
 
+	mkdir -p "npm/$package_name"
+
+	# Copy binary into build package directory
 	mkdir -p "npm/$package_name/bin"
 	cp "$rust_target_path$rust_target_name" "npm/$package_name/bin/$rust_target_name"
 	if [ "$node_platform" != "win32" ]; then
 		chmod +x "npm/$package_name/bin/$rust_target_name"
 	fi
 
+	# Create build package.json
+	cp "npm/package.json" "npm/$package_name/package.json"
+	sed -i "s/FIXME_VERSION/$1/g" "npm/$package_name/package.json"
+	sed -i "s/FIXME_BINARY/$package_name/g" "npm/$package_name/package.json"
+	sed -i "s/FIXME_PLATFORM/$platform_label/g" "npm/$package_name/package.json"
+	sed -i "s/FIXME_NODE_PLATFORM/$node_platform/g" "npm/$package_name/package.json"
+	sed -i "s/FIXME_ARCHITECTURE/$architecture/g" "npm/$package_name/package.json"
 	sed -i "s/FIXME_EXECUTABLE/$rust_target_name/g" "npm/$package_name/package.json"
+
+	# Create build README.md
+	cp "npm/README.md" "npm/$package_name/README.md"
+	sed -i "s/FIXME_RUST_TARGET/$rust_target_name/g" "npm/$package_name/package.json"
 done
